fba14d46683a526ebbca8ab803350532ff5e3fd0
Mike Hanssen <m.hanssen@gen25.com>
2020-07-14 21:26:36 +0200

    Removed the idea directory and added the vercel.json

--- api/rsvp.py
+++ api/rsvp.py
@@ -1,5 +1,30 @@
+import os
+import uuid
+
 from flask import Flask, Response
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+from sqlalchemy.dialects.postgresql import UUID
+
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = \
+    os.environ.get('DATABASE_URL', 'postgres://wedding@127.0.0.1/wedding')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+
+
+class Guests(db.Model):
+    __tablename__ = 'guests'
+
+    id = db.Column(
+        UUID(as_uuid=True), primary_key=True, default=uuid.uuid4, unique=True,
+        nullable=False)
+    name = db.Column(db.String())
+    attends = db.Column(db.Boolean(), default=False)
+    phone_number = db.Column(db.String(), nullable=True)
+
 
 @app.route('/', defaults={'path': ''})
 @app.route('/<path:path>')
